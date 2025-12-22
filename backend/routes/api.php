dc1a6ace049bbb43de585a4b91a0b0dd88e19329
Aarik Maharjan <mhrznaarik870@gmail.com>
2025-12-22 12:09:28 +0545

    feat: update product routes middleware to include admin check

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,7 +10,7 @@ Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 
 
-Route::middleware(['auth:api'])->prefix('/products')->group(function () {
+Route::middleware(['auth:api, check.admin'])->prefix('/products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
     Route::post('/', [ProductController::class, 'store']);
     Route::match(['put', 'patch'], '/{id}', [ProductController::class, 'update']);
